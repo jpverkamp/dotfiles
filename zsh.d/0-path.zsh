cdb935772be83ecc38547fab7254bf3af687fb16
JP Verkamp <me@jverkamp.com>
2015-04-22 19:38:19 -0700

    More for iphone backup

--- zsh.d/0-path.zsh
+++ zsh.d/0-path.zsh
@@ -1,5 +1,5 @@
 # Add a directory for any custom binaries/libraries on this machine
-PATH=$HOME/.bin:$PATH
+PATH=$HOME/.bin:$HOME/.bin/iphone-backup:$PATH
 export PATH
 
 LD_LIBRARY_PATH=$HOME/.lib:$LD_LIBRARY_PATH
