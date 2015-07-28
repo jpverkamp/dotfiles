e67462ed42d53524ec08ea54e497f832cd3ce30c
JP Verkamp <me@jverkamp.com>
2015-07-28 12:04:44 -0700

    Update 1-alias.zsh

--- zsh.d/1-alias.zsh
+++ zsh.d/1-alias.zsh
@@ -44,3 +44,13 @@ docker-nuke () {
   docker rm -v $(docker ps -a -q -f status=exited);
   docker rmi $(docker images -f "dangling=true" -q);
 }
+
+boot2docker-reset () {
+  boot2docker destroy
+  boot2docker init
+  boot2docker start
+  $(boot2docker shellinit)
+  boot2docker ssh "echo 'EXTRA_ARGS=\"--insecure-registry registry.edmodo.io\"' | sudo tee -a /var/lib/boot2docker/profile" 
+  boot2docker stop
+  boot2docker start
+}
