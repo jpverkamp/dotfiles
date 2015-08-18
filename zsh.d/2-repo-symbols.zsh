f3c5de16926019f195931fe747166738beed314a
JP Verkamp <me@jverkamp.com>
2015-08-17 23:33:45 -0700

    Add last run smiley, clean up vcs integration with vcs_info

--- zsh.d/2-repo-symbols.zsh
+++
@@ -1,4 +0,0 @@
-Z_REPO_ALL_GOOD="✓"
-Z_REPO_NEW_FILES="⁂"                                                                      
-Z_REPO_CHANGES="✗"                                                                      
-Z_REPO_UNKNOWN="⁇" 
