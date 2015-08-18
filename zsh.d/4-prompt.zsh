7e316e1b1f92e616cbc8ba106aa710dec9ccf3e3
JP Verkamp <me@jverkamp.com>
2015-08-17 23:33:45 -0700

    Add last run smiley, clean up vcs integration with vcs_info

--- zsh.d/4-prompt.zsh
+++ zsh.d/4-prompt.zsh
@@ -1,13 +1,34 @@
+# Add current repo info to the prompt
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:git*' formats "%{$Z_CLR_BLUE%}%s %b%{$Z_CLR_RESET%}%m%u%c"
+
+local Z_DATE="%D{%Y-%m-%d %H:%M}"
+local Z_DIRECTORY="%{$Z_CLR_GREEN%}%~%{$Z_CLR_RESET%}"
+local Z_MACHINE="%{$Z_CLR_MAGENTA%}%n%{$Z_CLR_RESET%}@%{$Z_CLR_YELLOW%}%m%{$Z_CLR_RESET%}"
+local Z_STATUS="%(?,%{$Z_CLR_GREEN%}^_^%f%b,%{$Z_CLR_RED%};_;%f%b)"
+local Z_VCS=""
+
+precmd() {
+    vcs_info
+    echo "run"
+    if [[ -n ${vcs_info_msg_0_} ]]; then
+        Z_VCS="{${vcs_info_msg_0_}} "
+    else
+        Z_VCS=""
+    fi
+}
+
 # Automatically run an ls after each cd (or otherwise changing directory)
 function chpwd() {
     emulate -LR zsh
     ls
 }
 
-# Prompt may change
+# Control the prompt(s)
 function zsh_update_prompt {
-    PROMPT="
-┌ ☺ %{$Z_CLR_MAGENTA%}%n%{$Z_CLR_RESET%}@%{$Z_CLR_YELLOW%}%m$Z_GIT$Z_SVN %{$Z_CLR_GREEN%}%~%{$Z_CLR_RESET%}
+PROMPT="
+┌ ${Z_STATUS} ${Z_MACHINE} ${Z_VCS}${Z_DIRECTORY}
 └ "
 }
 precmd_functions+='zsh_update_prompt'
