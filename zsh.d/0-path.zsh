1f0ded684ca561f87d0b28cdc02ac53416f95122
JP Verkamp <me@jverkamp.com>
2015-09-14 15:48:02 -0700

    Update 0-path.zsh

--- zsh.d/0-path.zsh
+++ zsh.d/0-path.zsh
@@ -1,6 +1,10 @@
 # Add a directory for any custom binaries/libraries on this machine
-PATH=$HOME/.bin:$HOME/.bin/iphone-backup:$PATH
-export PATH
+export PATH=$HOME/.bin:$HOME/.bin/iphone-backup:$PATH
 
+# Set up Go
+export GOPATH=~/Projects/go
+export PATH=$PATH:$GOPATH/bin
+
+# Add any custom libraries
 LD_LIBRARY_PATH=$HOME/.lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
