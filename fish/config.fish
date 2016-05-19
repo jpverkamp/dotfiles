9e378a69cbbcef1ca2c87f491403115aaf8d9b4a
JP Verkamp <me@jverkamp.com>
2016-05-19 15:21:20 -0400

    Set editor, add whitespace

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
-# Fish git prompt
 set __fish_git_prompt_color_branch blue
 
 set -U fish_user_paths $fish_user_paths ~/.bin
+set -U EDITOR vim
+
+function whitespace_after_prompt --on-event fish_preexec
+    printf "\n"
+end
