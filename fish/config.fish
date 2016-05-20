5f6f9cb80b0cd454398c0efd549fec7b70bedfb2
JP Verkamp <me@jverkamp.com>
2016-05-20 15:23:55 -0400

    Set up go

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,9 @@ set __fish_git_prompt_color_branch blue
 set -U fish_user_paths $fish_user_paths ~/.bin
 set -U EDITOR vim
 
+set -U GOPATH=~/Projects/go
+set -U fish_user_paths $fish_user_paths $GOPATH/bin
+
 function whitespace_after_prompt --on-event fish_preexec
     printf "\n"
 end
