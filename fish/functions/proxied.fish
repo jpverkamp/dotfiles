a3e9f73e7f6112c666b01b98586a9e7e0e8159c8
JP Verkamp <me@jverkamp.com>
2016-12-02 12:36:01 -0500

    Add function to run a command via my polipo proxy on port 7770

---
+++ fish/functions/proxied.fish
@@ -0,0 +1,11 @@
+function proxied
+    if ps aux | grep polipo | grep -v grep > /dev/null
+        set -lx http_proxy localhost:7770
+        set -lx https_proxy localhost:7770
+
+        eval $argv
+    else
+        echo "Polipo is not running"
+        exit
+    end
+end
