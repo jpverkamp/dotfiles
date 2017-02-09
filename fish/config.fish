554e8490dbea4808dc8f755f7a8511a6b5f46f32
JP Verkamp <me@jverkamp.com>
2017-02-09 17:16:20 -0500

    Only use global variables instead of universal to fix a warning

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,14 @@
 set __fish_git_prompt_color_branch blue
 
-set -Ux fish_user_paths $fish_user_paths ~/.bin
-set -Ux EDITOR vim
+set -gx fish_user_paths $fish_user_paths ~/.bin
+set -gx EDITOR vim
 
-set -Ux GOPATH ~/Projects/go
-set -Ux fish_user_paths $fish_user_paths $GOPATH/bin
+set -gx GOPATH ~/Projects/go
+set -gx fish_user_paths $fish_user_paths $GOPATH/bin
 
 function whitespace_after_prompt --on-event fish_preexec
     printf "\n"
 end
 
 # Directory list in cyan rather than a hard to see blue
-set -Ux LSCOLORS gxfxcxdxbxegedabagacad
+set -gx LSCOLORS gxfxcxdxbxegedabagacad
