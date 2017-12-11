c2f763556a019360fbd7456cb8c189feaecb260e
JP Verkamp <me@jverkamp.com>
2017-12-11 10:55:50 -0500

    Syncing from laptop

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ set -gx fish_user_paths $fish_user_paths ~/.bin
 set -gx EDITOR vim
 
 set -gx GOPATH ~/Projects/go
-set -gx fish_user_paths $fish_user_paths $GOPATH/bin
+#set -gx fish_user_paths $fish_user_paths $GOPATH/bin
 
 function whitespace_after_prompt --on-event fish_preexec
     printf "\n"
@@ -13,8 +13,3 @@ end
 # Directory list in cyan rather than a hard to see blue
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
-
-# Ruby yo
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
