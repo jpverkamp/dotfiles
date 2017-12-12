7065334a119b48fd0f0895ba7e279a02793b9978
JP Verkamp <me@jverkamp.com>
2017-12-12 11:55:01 -0500

    Replaced proxied with more powerful autoproxied command

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,6 @@ end
 # Directory list in cyan rather than a hard to see blue
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
+set -gx AUTOPROXY_HTTP_PROXY 192.168.0.50:7770
+set -gx AUTOPROXY_HTTPS_PROXY 192.168.0.50:7770
+alias ec2="autoproxied --condition='dig +short myip.opendns.com @resolver1.opendns.com | egrep \"^(10|50|52)\\.\"' --invert ec2"
