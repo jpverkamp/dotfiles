361cd51ad7bce1265153b3d9eccbeab770770258
JP Verkamp <me@jverkamp.com>
2023-03-17 13:38:45 -0400

    Don't comment out all your work and then commit it... :p

--- install.sh
+++ install.sh
@@ -11,25 +11,25 @@ else
 fi
 
 # Install nix packages
-# nix-env -iA nixpkgs.age
-# nix-env -iA nixpkgs.awscli
-# nix-env -iA nixpkgs.cargo
-# nix-env -iA nixpkgs.dasel
-# nix-env -iA nixpkgs.diceware
-# nix-env -iA nixpkgs.graphviz
-# nix-env -iA nixpkgs.htop
-# nix-env -iA nixpkgs.hugo
-# nix-env -iA nixpkgs.jq
-# nix-env -iA nixpkgs.python3
-# nix-env -iA nixpkgs.reattach-to-user-namespace
-# nix-env -iA nixpkgs.rustc
-# nix-env -iA nixpkgs.silver-searcher
-# nix-env -iA nixpkgs.tldr
-# nix-env -iA nixpkgs.tmux
-# nix-env -iA nixpkgs.tree
-# nix-env -iA nixpkgs.yarn
-# nix-env -iA nixpkgs.yq
-# nix-env -u
+nix-env -iA nixpkgs.age
+nix-env -iA nixpkgs.awscli
+nix-env -iA nixpkgs.cargo
+nix-env -iA nixpkgs.dasel
+nix-env -iA nixpkgs.diceware
+nix-env -iA nixpkgs.graphviz
+nix-env -iA nixpkgs.htop
+nix-env -iA nixpkgs.hugo
+nix-env -iA nixpkgs.jq
+nix-env -iA nixpkgs.python3
+nix-env -iA nixpkgs.reattach-to-user-namespace
+nix-env -iA nixpkgs.rustc
+nix-env -iA nixpkgs.silver-searcher
+nix-env -iA nixpkgs.tldr
+nix-env -iA nixpkgs.tmux
+nix-env -iA nixpkgs.tree
+nix-env -iA nixpkgs.yarn
+nix-env -iA nixpkgs.yq
+nix-env -u
 
 # Disable sticky keys on Mac
 if [ $SYSTEM = darwin ]; then
